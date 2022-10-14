ff1530136349d4fe4d4e3332fd0ac3f49ce24a83
PortalRomOS <portalromOS@gmail.com>
2022-10-14 10:48:43 +0000

    Change to PortalROm

---
+++ src/org/portalrom/portalromparts/notificationlight/BatteryBrightnessZenPreference.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2022 The Portal Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.portalrom.portalromparts.notificationlight;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.UserHandle;
+import android.util.AttributeSet;
+
+import portalrom.providers.PortalRomSettings;
+
+public class BatteryBrightnessZenPreference extends BrightnessPreference {
+    private static final String TAG = "BatteryBrightnessZenPreference";
+
+    private final ContentResolver mResolver;
+
+    public BatteryBrightnessZenPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mResolver = context.getContentResolver();
+    }
+
+    @Override
+    protected int getBrightnessSetting() {
+        return PortalRomSettings.System.getIntForUser(mResolver,
+                PortalRomSettings.System.BATTERY_LIGHT_BRIGHTNESS_LEVEL_ZEN,
+                LIGHT_BRIGHTNESS_MAXIMUM, UserHandle.USER_CURRENT);
+    }
+
+    @Override
+    protected void setBrightnessSetting(int brightness) {
+        PortalRomSettings.System.putIntForUser(mResolver,
+                PortalRomSettings.System.BATTERY_LIGHT_BRIGHTNESS_LEVEL_ZEN,
+                brightness, UserHandle.USER_CURRENT);
+    }
+}
---
+++ src/org/portalrom/portalromparts/profiles/actions/item/DisabledItem.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2022 The Portal Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.portalrom.portalromparts.profiles.actions.item;
+
+import android.content.Context;
+
+public class DisabledItem extends Item {
+    private final int mResTitle;
+    private final int mResSummary;
+
+    public DisabledItem(int resTitle, int resSummary) {
+        mResTitle = resTitle;
+        mResSummary = resSummary;
+    }
+
+    @Override
+    public String getTitle(Context context) {
+        return context.getString(mResTitle);
+    }
+
+    @Override
+    public String getSummary(Context context) {
+        return context.getString(mResSummary);
+    }
+
+    @Override
+    public boolean isEnabled(Context context) {
+        return false;
+    }
+}
